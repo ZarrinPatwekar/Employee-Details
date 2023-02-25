68ade40fb03d38bf20e946a0424192dd1f9c964c
ZarrinPatwekar <zpatwekar@gmail.com>
2023-02-26 03:03:13 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import  Employee  from "./MyComponent/Employee";
+function App() {
+  return (
+    <>
+    <Employee />
+    </>
+  );
+}
+
+export default App;
